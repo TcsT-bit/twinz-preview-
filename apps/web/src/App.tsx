970519c51bab294a4ab7549951088fd659961ded
TcsT-bit <thabang1givenkuna@gmail.com>
2026-07-02 15:34:29 +0000

    GOD TIER: Full stack bootstrap v2

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,3 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Login } from './routes/Login'
+export default function App(){return <Routes><Route path="/login" element={<Login/>}/><Route path="*" element={<Navigate to="/login"/>}/></Routes>}
