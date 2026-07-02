f8e5356ca821b298afd1d2428a0552a42e2c5dd9
TcsT-bit <thabang1givenkuna@gmail.com>
2026-07-02 17:28:43 +0200

    Delete apps/web directory

--- apps/web/src/App.tsx
+++
@@ -1,3 +0,0 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { Login } from './routes/Login'
-export default function App(){return <Routes><Route path="/login" element={<Login/>}/><Route path="*" element={<Navigate to="/login"/>}/></Routes>}
